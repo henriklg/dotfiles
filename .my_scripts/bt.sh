c5f1541402e5f624312bf389815c2b390fddd8ac
Henrik Gjestang <henrik.logj@gmail.com>
2020-09-01 11:20:39 +0200

    run bt in background

--- .my_scripts/bt.sh
+++ .my_scripts/bt.sh
@@ -3,7 +3,7 @@
 # used for connecting to bluetooth headset
 function bt() {
   echo "Connecting to headphones"
-  bluetoothctl connect 2C:41:A1:49:82:12
+  bluetoothctl connect 2C:41:A1:49:82:12 &
 }
 
 
