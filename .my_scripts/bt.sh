1beea463ae212f8b29a292a5e944751a320f0efd
Henrik Gjestang <henrik.logj@gmail.com>
2020-09-01 11:14:53 +0200

    removed master-thesis-script and changed scroll-script to blutetooth-script

---
+++ .my_scripts/bt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# used for connecting to bluetooth headset
+function bt() {
+  echo "Connecting to headphones"
+  bluetoothctl connect 2C:41:A1:49:82:12
+}
+
+
+
--- .my_scripts/master.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-function master() {
-  cd master-thesis/code
-  conda activate TF2
-  jupyter-lab
-}
