fcf8f5543384c90911d42ca35a8f462d6a7c3ffe
Henrik Gjestang <henrik.gjestang@optimeering.com>
2021-09-24 17:13:11 +0200

    pull from new branch, added variable for origin shell

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,29 +3,33 @@
 # pull in the latest version and copy the files to your home folder.
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
+git pull origin merge_zsh;
+
+MYSHELL=$(readlink /proc/$$/exe | sed -e 's/\/.*\///g')
+echo "Currently on ${MYSHELL}."
 
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		-avh --no-perms . ~;
-
-	source ~/.bash_profile;
+	source ~/.${MYSHELL}_profile;
 	
 	# add source bash_profile in bashrc
-	addprofile="if [ -f ~/.bash_profile ]; then . ~/.bash_profile; fi"
+	addprofile="if [ -f ~/.${MYSHELL}_profile ]; then . ~/.${MYSHELL}_profile; fi"
 	
 	# checks all lines
-	if ! grep -Fxq "$addprofile" ~/.bashrc; then
-		echo $addprofile >> ~/.bashrc
+	if ! grep -Fxq "$addprofile" ~/.$($MYSHELL)rc; then
+		echo $addprofile >> ~/.$($MYSHELL)rc
 	fi
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [ "$1" = "--force" -o "$1" = "-f" ]; then
 	doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo -n "This may overwrite existing files in your home directory. Are you sure? (y/n)? " # works in zsh and bash
+	read REPLY 
+	#read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1; # only works in bash
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt;
