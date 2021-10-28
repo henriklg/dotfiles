d01665f9399c74c37692c749665b61b1f79f11c1
Henrik Gjestang <henrik.logj@gmail.com>
2021-10-28 14:59:04 +0200

    init commit

---
+++ one_time_setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#TODO:
+# 1. install pyenv, poetry, zsh, exa
+# 2. set environment variables in bashrc and zshrc
+# 3. configure fonts, bootstrap zshrc and bashrc
+# 4. install oh-my-zsh and powerline, and configure
+# 5. install and enable powerline addons
